fee9ca7838348baff2eb02a6b8bb4dadc015d981
Alexander Krylovsky PC <cryls@yandex.ru>
2024-11-17 13:56:17 +0300

    unfinished static layout - v.0.2.1

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import HeaderBar from '@/components/HeaderBar.vue'
+import MainTable from '@/components/MainTable.vue'
+import TabsBar from '@/components/TabsBar.vue'
+
+export default {
+  components: {
+    HeaderBar,
+    MainTable,
+    TabsBar,
+  },
+
+  data() {
+    return {}
+  },
+
+  methods: {},
+}
+</script>
+
+<template>
+  <HeaderBar />
+  <MainTable />
+  <TabsBar />
+</template>
+
+<style src="@/styles/components/App.scss"></style>
